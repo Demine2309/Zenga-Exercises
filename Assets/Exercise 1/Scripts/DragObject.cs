9df0b00cb33441c8228aff90f91a4d0ac1702db0
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-05-27 12:24:16 +0700

    Zenga Exercises
    27/5/2024

---
+++ Assets/Exercise 1/Scripts/DragObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Camera mainCamera;
+    private bool isDragging = false;
+    private Vector3 offset;
+    private float zCoordinate;
+    private float fixedY; 
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        fixedY = transform.position.y;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.transform == transform)
+            {
+                isDragging = true;
+                zCoordinate = mainCamera.WorldToScreenPoint(transform.position).z;
+                offset = transform.position - GetMouseWorldPosition();
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+            isDragging = false;
+
+        if (isDragging)
+            transform.position = new Vector3(GetMouseWorldPosition().x + offset.x, fixedY, GetMouseWorldPosition().z + offset.z);
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zCoordinate;
+
+        return mainCamera.ScreenToWorldPoint(mousePoint);
+    }
+}
